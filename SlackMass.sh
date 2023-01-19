f3e12128be1cd62573697e02cc6802e49ec92019
prune2000 <c.victor.p@protonmail.com>
2023-01-19 14:36:50 -0500

    Add the option to change the Amass local folder

--- SlackMass.sh
+++ SlackMass.sh
@@ -7,13 +7,17 @@ YEL="\e[93m"
 
 read -p "Please enter the domain Name: " domain
 read -p "Please enter your Slack webhook: " webhook
+read -p "Please enter the location of your Amass database (f.ex.: /~/.config/amass): " db
 
 # Catch error
 if [ -z "$domain" ]; then
-        echo "$RED[+] ERROR: You have not entered the Domain Name."
-        exit 1
+    echo "$RED[+] ERROR: You have not entered the Domain Name."
+    exit 1
 elif [ -z "$webhook" ]; then
     echo "$RED[+] ERROR: You have not entered the webhook"
+    exit 1
+elif [ -z "$db" ]; then
+    echo "$RED[+] ERROR: You have not entered the Amass database"
     exit 1
 else
     echo "$BLU[+] Starting Amass on$RED $domain"
@@ -28,7 +32,7 @@ while true
     echo '\n'
     cat enum.txt
     echo '\n'
-    amass track -dir /home/kali/.config/amass -d $domain -last 2
+    amass track -dir $db -d $domain -last 2
     if [ $? -eq 1 ]; then
         echo "$BLU[+] It was your first time running Amass on$RED $domain, sending results to Slack"
         cat enum.txt | ./slackcat -u $webhook >/dev/null
@@ -36,7 +40,7 @@ while true
     else 
         echo  "$BLU[+] Comparing results with previous Amass enum"
         # Check if there are changes to not spam Slack
-        amass track -dir /home/kali/.config/amass -d $domain -last 2 > $domain.txt
+        amass track -dir $db -d $domain -last 2 > $domain.txt
         if cat $domain.txt | grep -q "No differences discovered"; then
             echo "$BLU[+] No differences discovered"
             rm -rf $domain.txt
